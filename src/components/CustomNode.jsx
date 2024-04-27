c011d4c2e9610da5f679717dec74fc245ae5da65
cliffordmarkmusni <cliffordmarkmusni@gmail.com>
2024-04-27 14:19:52 +0800

    Removed unused codes on CustomNode

--- src/components/CustomNode.jsx
+++ src/components/CustomNode.jsx
@@ -1,15 +1,8 @@
-import { useCallback } from 'react';
 import { Handle, Position } from 'reactflow';
 import CardFLip from './card/CardFlip';
 import './custom-node.css';
 
-const handleStyle = { left: 10 };
-
-function CustomNode({ data, isConnectable }) {
-  const onChange = useCallback((evt) => {
-    console.log(evt.target.value);
-  }, []);
-
+function CustomNode({ data }) {
   return (
     <div className="custom-node">
       <CardFLip id={data.id} label={data.label} isFlipped={true} />
